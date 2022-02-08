9d01476dbcef213b1fd6beb9d5d27a697efc52c4
TingeyTime <ntingey@gmail.com>
2022-02-08 16:53:33 -0700

    Change to NUXT

---
+++ api/server.js
@@ -0,0 +1,34 @@
+const Enforcer = require('openapi-enforcer')
+const EnforcerMiddleware = require('openapi-enforcer-middleware')
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+// Any paths defined in your openapi.yml will validate and parse the request
+// before it calls your route code.
+const openapiPath = path.resolve(__dirname, '../openapi.yml')
+const enforcer = await Enforcer(openapiPath, { hideWarnings: true })
+const enforcerMiddleware = EnforcerMiddleware(enforcer)
+
+//bodyParsers
+app.use(express.json())
+app.use(express.text())
+
+// Print log server-side
+app.use((req, res, next) => {
+  // console.log(req.method + ' ' + req.path, req.headers, req.body)
+  next()
+})
+
+app.use(enforcerMiddleware.init())
+
+// Catch errors
+enforcerMiddleware.on('error', err => {
+  console.error(err)
+  process.exit(1)
+}) 
+
+app.use(enforcerMiddleware.mock())
+
+module.exports = app
