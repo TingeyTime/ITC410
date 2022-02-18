ac60f5125cd3a71dd67e0a1dd26b1a810fccf696
TingeyTime <ntingey@gmail.com>
2022-02-17 19:53:15 -0700

    add Account schema backbone

--- api/models/account.js
+++ api/models/account.js
@@ -49,11 +49,31 @@ AccountSchema.path('email').validate(function(email) {
 
 AccountSchema.path('email').validate(function(email) {
     return new Promise(resolve => {
+	const Account = mongoose.model('Account');
+	if (this.skipValidation()) return resolve(true);
 
-    })
-})
+	// Check only when it is a new account or when email field is modified
+	if (this.isNew || this.isModified('email')) {
+	    Account.find({ email }).exec((err, accounts) => resolve(!err && !users.length)
+	} else resolve(true);
+    });
+}, 'Email `{VALUE} already exists');
 
 AccountSchema.path('name').validate(function(name) {
     if (this.skipValidation()) return true;
     return name.length;
 }, 'Name cannot be blank')
+
+/**
+ * Methods
+ */
+
+UserSchema.methods = {
+	/**
+	 * Authenticate - check if the passwords are the same
+	 * Encrypt
+	 * Validation
+	 */
+};
+
+mongoose.model('Account', AccountSchema);
