a8ded5d1213aecd4c6492088b83585fbdee2d487
TingeyTime <ntingey@gmail.com>
2022-02-17 17:15:14 -0700

    add more routes

---
+++ api/models/Account.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// Follow https://github.com/madhums/node-express-mongoose-demo/blob/master/app/models/user.js
+
+const { Schema } = mongoose;
+
+/**
+ * Account Schema
+ */
+
+const AccountSchema = new Schema({
+  account_id: { type: String },
+  username: { type: String, default: '' },
+  email: { type: String, default: '' },
+  name: { type: String, default: '' },
+  password: { type: String, default: '' },
+  session_info: []
+});
+
+const validatePresenceOf = value => value && value.length;
+
+/**
+ * Virtuals
+ */
+
+AccountSchema.virtual('password')
+    .set(function(password) {
+        this._password = password;
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function() {
+        return this._password;
+    });
+
+/**
+ * Validations
+ */
+
+AccountSchema.path('username').validate(function(username) {
+    if (this.skipValidation()) return true;
+    return username.length;
+}, 'Username cannot be blank');
+
+AccountSchema.path('email').validate(function(email) {
+    if (this.skipValidation()) return true;
+    return email.length;
+}, 'Email cannot be blank');
+
+AccountSchema.path('email').validate(function(email) {
+    return new Promise(resolve => {
+
+    })
+})
+
+AccountSchema.path('name').validate(function(name) {
+    if (this.skipValidation()) return true;
+    return name.length;
+}, 'Name cannot be blank')
